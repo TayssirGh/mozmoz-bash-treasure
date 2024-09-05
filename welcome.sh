3cd494a66ed16c88421c3865887147ee0ba5251f
tayssir <tayssir.ghanmi@eniso.u-sousse.tn>
2024-09-05 16:59:44 +0100

    [UPDATED] welcome to the game

--- welcome.sh
+++ welcome.sh
@@ -2,33 +2,34 @@
 
 welcome_file="welcome"
 RED='\033[0;31m'
-GREEN='\033[0;32m'
+YELLOW='\033[\0;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 welcome_message="
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+${BLUE}~~~~~~~~~~~~~~~~${YELLOW}~~~~~~~~~~~~~~~~~~~~${RED}~~~~~~~~~~~~~~~~~~~~~~~${NC}
+                __
 __  _  __ ____ |  |   ____  ____   _____   ____
 \ \/ \/ _/ __ \|  | _/ ___\/  _ \ /     \_/ __ \  
  \     /\  ___/|  |_\  \__(  <_> |  Y Y  \  ___/  
   \/\_/  \___  |____/\___  \____/|__|_|  /\___  > 
-             \/          \/            \/     \/ 
+             \/          \/            \/     \/
                                          
 
-                 🧙‍♂️ Welcome, Brave Adventurer! 🧙‍♂️
+         🧙‍♂️ Welcome, Brave Adventurer! 🧙‍♂️
 
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+${BLUE}~~~~~~~~~~~~~~~~${YELLOW}~~~~~~~~~~~~~~~~~~~~${RED}~~~~~~~~~~~~~~~~~~~~~~~${NC}
           The wise wizard Mozmoz greets you warmly.
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-You have embarked on a grand quest to find the hidden treasure 
+${BLUE}~~~~~~~~~~~~~~~~${YELLOW}~~~~~~~~~~~~~~~~~~~~${RED}~~~~~~~~~~~~~~~~~~~~~~~${NC}
+You have stepped into a grand quest to find the hidden treasure
 that lies within the mysterious depths of the Linux directories.
 But beware! The path is treacherous, filled with riddles and 
 challenges that only the cleverest of minds can solve.
+${RED}cd${NC}, ${RED}ls${NC} and ${RED}cat${NC} are all you need to know to start playing,
+and ALWAYS READ THE ${RED}GRIMORES${NC} FIRST, THEY CONTAIN IMPORTANT INFO!
 
-  May your journey be filled with courage, wisdom, and luck.
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+May your journey be filled with courage, wisdom, and luck.
+${BLUE}~~~~~~~~~~~~~~~~${YELLOW}~~~~~~~~~~~~~~~~~~~~${RED}~~~~~~~~~~~~~~~~~~~~~~~~~${NC}
 "
+echo -e "$welcome_message" > "$welcome_file"
 
-echo "$welcome_message" > "$welcome_file"
-
-echo "Welcome file created: $welcome_file"
+cat $welcome_file
