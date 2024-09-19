599afe4808f638225380448fbb8c57f77daa7918
tayssir <tayssir.ghanmi@eniso.u-sousse.tn>
2024-09-19 17:24:24 +0100

    [UPDATED] made some adjustments in the colors and spacings

--- the-woodland-gate/.first_glade/.second_glade/gate.sh
+++ the-woodland-gate/.first_glade/.second_glade/gate.sh
@@ -4,14 +4,14 @@
 lock_val=$(cat secret-door/book-of-knowledge  | wc -l)
 
 cat <<EOF >spell
-[0;34m##############[0;33m################[1;31m#################[0m
+[0;34m##############[1;33m################[1;31m#################[0m
 [0;34m#[0m « cat filename  | wc -l »                   [1;31m#[0m
 [0;34m#[0m                                             [1;31m#[0m
 [0;34m#[0m There are many ways to count words or lines [1;31m#[0m
 [0;34m#[0m                                             [1;31m#[0m
 [0;34m#[0m in the shell. This is one of them.          [1;31m#[0m
 [0;34m#[0m                                             [1;31m#[0m
-[0;34m##############[0;33m################[1;31m#################[0m
+[0;34m##############[1;33m################[1;31m#################[0m
 EOF
 
 echo -e "This chest is locked!\nYou need the proper key to enter!"
@@ -28,7 +28,7 @@ else
 fi
 
 cat <<EOF >gold
-[0;34m####################[0;33m######################[1;31m####################[0m
+[0;34m####################[1;33m######################[1;31m####################[0m
 [0;34m#[0m Here are 10 gold coins.                                     [1;31m#[0m
 [0;34m#[0m                                                             [1;31m#[0m
 [0;34m#[0m Create a « gold » variable and asign the value of 10 to it. [1;31m#[0m
@@ -38,7 +38,7 @@ cat <<EOF >gold
 [0;34m#[0m You can always check your gold with:                        [1;31m#[0m
 [0;34m#[0m                                                             [1;31m#[0m
 [0;34m#[0m echo \$gold                                                  [1;31m#[0m
-[0;34m####################[0;33m######################[1;31m####################[0m
+[0;34m####################[1;33m######################[1;31m####################[0m
 EOF
 
 mv .third_glade third_glade 2>/dev/null
